5119ff190ffcac57216f91c4841e27b8d72b522c
florentboutreux <florent.boutreux@gmail.com>
2026-09-17 12:25:10 +0200

    chasseur_immo.py

--- chasseur_immo.py
+++ chasseur_immo.py
@@ -18,7 +18,7 @@ def get_unread_alerts():
     mail.select("inbox")
     
     # CORRECTION : Syntaxe séparée pour la recherche Gmail
-    status, messages = mail.search(None, 'X-GM-RAW', 'is:unread subject:alerte')
+    status, messages = mail.search(None, b'X-GM-RAW', b'"is:unread subject:alerte"')
     urls_trouvees = []
     
     # On vérifie si la recherche a trouvé des emails
